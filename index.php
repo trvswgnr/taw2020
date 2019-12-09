9d6cc1df83f8a20ffe7c50d86d810b300140b233
trav <trvswgnr@gmail.com>
2019-12-09 17:15:05 -0500

    add default template

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Theme Default Template
+ *
+ * @package taw
+ */
+
+get_header();
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+		the_content();
+	endwhile;
+endif;
+get_footer();
