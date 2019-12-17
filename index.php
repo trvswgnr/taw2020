55767ea10bcdaee8e5b4f4d1a6a122dc2b21ff86
trav <trvswgnr@gmail.com>
2019-12-17 18:36:08 -0500

    add index.php

--- index.php
+++ index.php
@@ -6,10 +6,22 @@
  */
 
 get_header();
+?>
+<main class="container">
+<?php
 if ( have_posts() ) :
 	while ( have_posts() ) :
+		?>
+		<article class="content">
+		<?php
 		the_post();
 		the_content();
+		?>
+		</article>
+		<?php
 	endwhile;
 endif;
+?>
+</main>
+<?php
 get_footer();
