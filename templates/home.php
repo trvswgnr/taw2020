980edd8868e7240177bd9672928e184cfbdda69e
trav <trvswgnr@gmail.com>
2019-12-19 18:31:19 -0500

    add placeholder nav menu

--- templates/home.php
+++ templates/home.php
@@ -17,6 +17,16 @@ get_header();
 		<h1 class="spinner__heading">Travis A. Wagner</h1>
 		<p class="spinner__desc">Developer <span>/</span> Designer <span>/</span> Producer <span>/</span> Digital Marketer</p>
 	</div>
+	<nav class="nav-home">
+		<ul class="nav-home__menu">
+		<li><a href="#">Résumé</a></li>
+		<li><a href="#">Portfolio</a></li>
+		<li><a href="#">Articles</a></li>
+		<li><a href="#">Music</a></li>
+		<li><a href="#">About</a></li>
+		<li><a href="#">Contact</a></li>
+		</ul>
+	</nav>
 </div>
 <?php
 get_footer();
