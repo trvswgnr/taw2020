69719a2aa068f24b0da988b7095abd00439b055f
trav <trvswgnr@gmail.com>
2019-12-19 18:30:55 -0500

    remove title tag, use theme support title instead

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<title><?php echo 'Travis Wagner - ' . esc_html( get_the_title() ); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
