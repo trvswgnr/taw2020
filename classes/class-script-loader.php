019bdc7b12baea1a3913c202f195d26288f339d4
trav <trvswgnr@gmail.com>
2019-12-19 20:43:12 -0500

    add async scripts class

---
+++ classes/class-script-loader.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Javascript Loader Class
+ *
+ * Allow `async` and `defer` while enqueuing Javascript.
+ *
+ * @package taw2020
+ */
+
+if ( ! class_exists( 'Script_Loader' ) ) {
+	/**
+	 * A class that provides a way to add `async` or `defer` attributes to scripts.
+	 */
+	class Script_Loader {
+
+		/**
+		 * Adds async/defer attributes to enqueued / registered scripts.
+		 *
+		 * @param string $tag    The script tag.
+		 * @param string $handle The script handle.
+		 * @return string Script HTML string.
+		 */
+		public function filter_script_loader_tag( $tag, $handle ) {
+			foreach ( [ 'async', 'defer' ] as $attr ) { // phpcs:ignore
+				if ( ! wp_scripts()->get_data( $handle, $attr ) ) {
+					continue;
+				}
+				// Prevent adding attribute when already added in #12009.
+				if ( ! preg_match( ":\s$attr(=|>|\s):", $tag ) ) {
+					$tag = preg_replace( ':(?=></script>):', " $attr", $tag, 1 );
+				}
+				// Only allow async or defer, not both.
+				break;
+			}
+			return $tag;
+		}
+
+	}
+}
--- functions.php
+++ functions.php
@@ -5,9 +5,15 @@
  * @package taw
  */
 
-// get utility functions.
+// local utility functions.
 if ( file_exists( dirname( __FILE__ ) . '/functions-local.php' ) ) {
 	require_once dirname( __FILE__ ) . '/functions-local.php';
 }
-require_once 'classes/class-theme-setup.php';
+
+// async script loader class.
+require_once get_template_directory() . '/classes/class-script-loader.php';
+
+// theme setup.
+require_once get_template_directory() . '/classes/class-theme-setup.php';
+
 $taw2020 = new Theme_Setup();
