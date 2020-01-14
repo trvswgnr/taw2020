9cd3fcaaa5c9380ec5b33b9d03aab97012b9dab4
trav <trvswgnr@gmail.com>
2020-01-14 04:05:38 -0500

    add classes

--- functions.php
+++ functions.php
@@ -10,10 +10,15 @@ if ( file_exists( dirname( __FILE__ ) . '/functions-local.php' ) ) {
 	require_once dirname( __FILE__ ) . '/functions-local.php';
 }
 
-// async script loader class.
+// async script loader.
 require_once get_template_directory() . '/classes/class-script-loader.php';
 
 // theme setup.
 require_once get_template_directory() . '/classes/class-theme-setup.php';
 
-$taw2020 = new Theme_Setup();
+// blocks.
+require_once get_template_directory() . '/blocks/class-block.php';
+require_once get_template_directory() . '/blocks/resume/list-columns/class-list-columns.php';
+
+$taw                 = new Theme_Setup();
+$resume_list_columns = new Resume\List_Columns();
