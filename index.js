e57e377ec4434986dfbbf61ccc1c314c52d2626b
trav <trvswgnr@gmail.com>
2020-01-14 00:45:34 -0500

    add spinner js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 /**
  * Main JS
  */
+
+( function( $ ) {
+	$( '.spinner a' ).click( function() {
+		$( '.spinner' ).addClass( 'is-active' ).fadeOut( 1000 );
+	} );
+
+	$( document ).ready( function() {
+		if ( $( this ).scrollTop() <= 5 ) {
+			$( '#content' ).addClass( 'is-active' );
+		} else {
+			$( '#content' ).addClass( 'is-active no-transition' );
+		}
+	} );
+}( jQuery ) );
