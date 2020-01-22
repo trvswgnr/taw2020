88184ea223be46b798fe8c06f6d9555eeba70fd1
trav <trvswgnr@gmail.com>
2020-01-22 17:20:07 -0500

    add education block

--- functions.php
+++ functions.php
@@ -21,11 +21,13 @@ require_once 'blocks/class-block.php';
 require_once 'blocks/resume/list-columns/class-list-columns.php';
 require_once 'blocks/resume/experience/class-experience.php';
 require_once 'blocks/resume/chips/class-chips.php';
+require_once 'blocks/resume/education/class-education.php';
 
 $taw                 = new Theme();
 $resume_list_columns = new Resume\List_Columns();
 $resume_experience   = new Resume\Experience();
 $resume_chips        = new Resume\Chips();
+$resume_education    = new Resume\Education();
 
 Theme::remove_empty_p();
 Theme::add_block_category( 'Resume' );
