2933a14add9e721b992984b4ff3cae448ad7b073
trav <trvswgnr@gmail.com>
2020-01-22 17:20:48 -0500

    add resume id to is-active function

--- index.js
+++ index.js
@@ -9,9 +9,9 @@
 
 	$( document ).ready( function() {
 		if ( $( this ).scrollTop() <= 5 ) {
-			$( '#content' ).addClass( 'is-active' );
+			$( '#content, #resume' ).addClass( 'is-active' );
 		} else {
-			$( '#content' ).addClass( 'is-active no-transition' );
+			$( '#content, #resume' ).addClass( 'is-active no-transition' );
 		}
 	} );
 }( jQuery ) );
